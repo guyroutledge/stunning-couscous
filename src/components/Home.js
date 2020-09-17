95b5b893459bbc921435f803c766fba9e48dd1a8
Guy Routledge <mail@guyroutledge.co.uk>
2020-09-17 10:42:08 +0100

    More updates to home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,14 @@
 import React from 'react'
 
 const Home = () => {
+  const doSomething = () => {
+    return 'something'
+  }
   return (
     <>
       <div>This is a div</div>
       <h1>This is a header</h1>
+      <div>{doSomething()}</div>
     </>
   )
 }
